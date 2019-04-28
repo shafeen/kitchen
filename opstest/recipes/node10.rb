579771eeec98897679d59b495726b933045593c6
Shafeen M <sodl02@gmail.com>
2019-04-27 22:39:45 -0700

    update recipe opstest::node10 fixed typo

--- opstest/recipes/node10.rb
+++ opstest/recipes/node10.rb
@@ -3,7 +3,7 @@ bash "install_node10_nvm" do
     cwd "/home/ubuntu"
     environment ({'HOME' => '/home/ubuntu', 'USER' => 'ubuntu'})
     code <<-EOH
-        curl -o- https://raw.g  ithubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
         source ~/.bashrc
         nvm install 10.15.3
     EOH
