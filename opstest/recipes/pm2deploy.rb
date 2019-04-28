be8b84893127b9c0005fb93f9a7755dd1188becd
Shafeen M <sodl02@gmail.com>
2019-04-28 02:54:13 -0700

    updated opstest::pm2deploy for chef 12 recipes

--- opstest/recipes/pm2deploy.rb
+++ opstest/recipes/pm2deploy.rb
@@ -1,17 +1,20 @@
-node[:deploy].each do |application, deploy|
+app = search("aws_opsworks_app").first
 
-    bash "clone_app_repo" do
-        user "ubuntu"
-        group "ubuntu"
-        cwd "/home/ubuntu"
-        environment ({'HOME' => '/home/ubuntu', 'USER' => 'ubuntu'})
-        code <<-EOH
-            # add git deploy key to agent
-            echo "#{deploy[:scm][:ssh_key]}" | tr -d '\r' | ssh-add - > /dev/null
-            # clone the repo
-            git clone #{deploy[:scm][:repository]} #{application}
-        EOH
-    end
+Chef::Log.info("********** The app's short name is '#{app[:shortname]}' **********")
+Chef::Log.info("********** The app's URL is '#{app[:app_source][:url]}' **********")
 
-    # do more here later as part of the deploy task
+#  need to use data bags for chef 12 on OpsWorks
+bash "clone_app_repo" do
+    user "ubuntu"
+    group "ubuntu"
+    cwd "/home/ubuntu"
+    environment ({'HOME' => '/home/ubuntu', 'USER' => 'ubuntu'})
+    code <<-EOH
+        # add git deploy key to agent
+        echo "#{app[:app_source][:ssh_key]}" | tr -d '\r' | ssh-add - > /dev/null
+        # clone the repo
+        git clone #{app[:app_source][:url]} #{app[:shortname]}
+    EOH
 end
+
+# do more here later as part of the deploy task
