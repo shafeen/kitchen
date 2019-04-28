920d995a848f0cae99eeaa05b9e5f3524e1b6117
Shafeen M <sodl02@gmail.com>
2019-04-28 03:24:47 -0700

    updated opstest::pm2deploy fixed typo

--- opstest/recipes/pm2deploy.rb
+++ opstest/recipes/pm2deploy.rb
@@ -5,7 +5,7 @@ Chef::Log.info("********** The app's URL is '#{app[:app_source][:url]}' ********
 
 app_env = app[:environment]
 app_url = app[:app_source][:url]
-app_url = app_url.sub! "://" , "://#{app_env[:GITLAB_DEPLOY_USER]}:#{app_env[:GITLAB_DEPLOY_PASSWORD]}"
+app_url = app_url.sub! "://" , "://#{app_env[:GITLAB_DEPLOY_USER]}:#{app_env[:GITLAB_DEPLOY_PASSWORD]}@"
 
 #  need to use data bags for chef 12 on OpsWorks
 bash "clone_app_repo" do
