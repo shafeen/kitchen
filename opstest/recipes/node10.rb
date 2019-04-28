c47d2688d2200c2c86965e125d42d0d4bf155348
Shafeen M <sodl02@gmail.com>
2019-04-27 22:05:52 -0700

    update recipe opstest::node10

--- opstest/recipes/node10.rb
+++ opstest/recipes/node10.rb
@@ -1,6 +1,6 @@
 bash "install_node10_nvm" do
     user "ubuntu"
-    cwd "$HOME"
+    cwd "/home/ubuntu"
     code <<-EOH
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
         nvm install 10.15.3
