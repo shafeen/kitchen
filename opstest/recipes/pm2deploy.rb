33c7980c46c97175dc3a6580a5b8cd32e1e82104
Shafeen M <sodl02@gmail.com>
2019-04-28 03:22:33 -0700

    updated opstest::pm2deploy for chef 12 recipes

--- opstest/recipes/pm2deploy.rb
+++ opstest/recipes/pm2deploy.rb
@@ -3,6 +3,10 @@ app = search("aws_opsworks_app").first
 Chef::Log.info("********** The app's short name is '#{app[:shortname]}' **********")
 Chef::Log.info("********** The app's URL is '#{app[:app_source][:url]}' **********")
 
+app_env = app[:environment]
+app_url = app[:app_source][:url]
+app_url = app_url.sub! "://" , "://#{app_env[:GITLAB_DEPLOY_USER]}:#{app_env[:GITLAB_DEPLOY_PASSWORD]}"
+
 #  need to use data bags for chef 12 on OpsWorks
 bash "clone_app_repo" do
     user "ubuntu"
@@ -10,10 +14,8 @@ bash "clone_app_repo" do
     cwd "/home/ubuntu"
     environment ({'HOME' => '/home/ubuntu', 'USER' => 'ubuntu'})
     code <<-EOH
-        # add git deploy key to agent
-        echo "#{app[:app_source][:ssh_key]}" | tr -d '\r' | ssh-add - > /dev/null
-        # clone the repo
-        git clone #{app[:app_source][:url]} #{app[:shortname]}
+        # clone the repo using deploy tokens
+        git clone #{app_url} #{app[:shortname]}
     EOH
 end
 
