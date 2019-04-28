3fec8b54bcc9ba6b516a716e77d5d15a61420117
Shafeen M <sodl02@gmail.com>
2019-04-27 22:45:50 -0700

    update recipe opstest::node10 -- removing bashrc method

--- opstest/recipes/node10.rb
+++ opstest/recipes/node10.rb
@@ -4,7 +4,10 @@ bash "install_node10_nvm" do
     environment ({'HOME' => '/home/ubuntu', 'USER' => 'ubuntu'})
     code <<-EOH
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-        source ~/.bashrc
+        # source ~/.bashrc
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
         nvm install 10.15.3
     EOH
 end
