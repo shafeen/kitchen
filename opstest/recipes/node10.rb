2c9f13caac2b823327e84743c8205c07a7a76632
Shafeen M <sodl02@gmail.com>
2019-04-27 22:36:45 -0700

    update recipe opstest::node10 with env variables for user

--- opstest/recipes/node10.rb
+++ opstest/recipes/node10.rb
@@ -1,8 +1,10 @@
 bash "install_node10_nvm" do
     user "ubuntu"
     cwd "/home/ubuntu"
+    environment ({'HOME' => '/home/ubuntu', 'USER' => 'ubuntu'})
     code <<-EOH
-        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+        curl -o- https://raw.g  ithubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+        source ~/.bashrc
         nvm install 10.15.3
     EOH
 end
