8b330ca0c71fffd7f0b970a93674f5768877ed43
Shafeen M <sodl02@gmail.com>
2019-04-27 23:05:34 -0700

    updated opstest::npm_globals

--- opstest/recipes/npm_globals.rb
+++ opstest/recipes/npm_globals.rb
@@ -3,6 +3,11 @@ bash "install_npm_globals" do
     cwd "/home/ubuntu"
     environment ({'HOME' => '/home/ubuntu', 'USER' => 'ubuntu'})
     code <<-EOH
+        # ------ make sure nvm was installed first
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+        # ------ end loading nvm
         npm i -g gulp@3.9.1 pm2
     EOH
 end
