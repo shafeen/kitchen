22f73deacc76f97c3e466d502123eefdbf16632c
Shafeen M <sodl02@gmail.com>
2019-05-03 14:34:22 -0700

    update recipe opstest::python3

--- opstest/recipes/python3.rb
+++ opstest/recipes/python3.rb
@@ -3,3 +3,16 @@
         action :install
     end
 end
+
+# install the required pip3 modules
+["boto3"].each do |pip3_package_name|
+    bash "install pip3 package '#{pip3_package_name}'" do
+        user "ubuntu"
+        group "ubuntu"
+        cwd "/home/ubuntu"
+        environment ({'HOME' => '/home/ubuntu', 'USER' => 'ubuntu'})
+        code <<-EOH
+            pip3 install #{pip3_package_name}
+        EOH
+    end
+end
