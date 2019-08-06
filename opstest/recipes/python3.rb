ad8eb4fddcb0988b1b83ca5d920af8f86acd9063
Shafeen M <sodl02@gmail.com>
2019-08-05 23:09:10 -0700

    updated the "python3" setup recipe with pipenv

--- opstest/recipes/python3.rb
+++ opstest/recipes/python3.rb
@@ -1,10 +1,22 @@
-["python3-pip"].each do |package_name|
+# install virtualenv and pip3
+["python3-venv", "python3-pip"].each do |package_name|
     package package_name do
         action :install
     end
 end
 
-# install the required pip3 modules
+# install pipenv as a "user installation"
+bash "install pip3 pipenv" do
+    user "ubuntu"
+    group "ubuntu"
+    cwd "/home/ubuntu"
+    environment ({'HOME' => '/home/ubuntu', 'USER' => 'ubuntu'})
+    code <<-EOH
+        pip3 install --user pipenv
+    EOH
+end
+
+# install other required pip3 modules
 ["boto3"].each do |pip3_package_name|
     bash "install pip3 package '#{pip3_package_name}'" do
         user "ubuntu"
