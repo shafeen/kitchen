c1cb3e7848b0d975238fc82e742488ef8846e4e4
Shafeen Mahmud <sodl02@gmail.com>
2019-09-19 01:30:58 -0700

    added a python3_boto3 alternate python recipe

---
+++ opstest/recipes/python3_boto3.rb
@@ -0,0 +1,19 @@
+# install virtualenv and pip3
+["python3-venv", "python3-pip"].each do |package_name|
+    package package_name do
+        action :install
+    end
+end
+
+# install pipenv and any other required "user installation" pip3 modules
+["pipenv", "boto3"].each do |pip3_package_name|
+    bash "install pip3 package '#{pip3_package_name}'" do
+        user "ubuntu"
+        group "ubuntu"
+        cwd "/home/ubuntu"
+        environment ({'HOME' => '/home/ubuntu', 'USER' => 'ubuntu'})
+        code <<-EOH
+            pip3 install --user #{pip3_package_name}
+        EOH
+    end
+end
